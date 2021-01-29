2cb4b86ceb6430d5a102470450c5d34c0dd72814
Esra Erkan <esraerkan38@outlook.com>
2021-01-29 16:24:33 +0300

    Add project files.

---
+++ MyList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyList<C,B>
+    {
+        C[] _arrValue;
+        B[] _arrNumber;
+
+        public MyList()
+        {
+            _arrValue = new C[0];
+            _arrNumber = new B[0];
+        }
+        public void Add(C item1,B item2)
+        {
+            C[] _tempValue = _arrValue;
+            B[] _tempNumber = _arrNumber;
+            _arrValue = new C[_arrValue.Length + 1];
+            _arrNumber = new B[_arrNumber.Length + 1];
+
+            for (int i = 0; i <_tempValue.Length; i++)
+            {
+                _arrValue[i] = _tempValue[i];
+                _arrValue[_arrValue.Length - 1] = item1;
+            }
+            for (int i = 0; i < _tempNumber.Length; i++)
+            {
+                _arrNumber[i] = _tempNumber[i];
+                _arrNumber[_arrNumber.Length - 1] = item2;
+
+            }
+            Console.WriteLine( item1 + " " + " Ülke Barkod Kodu" + " " + "=" + " "  + item2 );
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string, int> UlkeBarkod = new MyList<string, int>();
+            UlkeBarkod.Add("Türkiye", 868 );
+            UlkeBarkod.Add("Güney Kıbrıs", 529);
+            UlkeBarkod.Add("Karadağ", 389);
+            UlkeBarkod.Add("Azerbaycan", 476);
+            UlkeBarkod.Add("Türkmenistan", 483);
+        }
+    }
+}
